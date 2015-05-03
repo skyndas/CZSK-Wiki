16dec69f7fa886afd7cbce6859e9ff3e4ed886f3
skyndas <skyndas@gmail.com>
2015-05-03 20:22:40 +0200

    First loading - test 2

---
+++ 404.php
@@ -0,0 +1,141 @@
+<!Doctype html>
+<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en-gb" class="no-js"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Priority - Multipurpose HTML5 Template</title>
+	
+	<meta name="description" content="">
+	<meta name="author" content="">
+	 
+	<!--[if lt IE 9]>
+	    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	  
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    
+    <!-- **CSS - stylesheets** -->
+	<link id="default-css" rel="stylesheet" href="style.css" type="text/css" media="all" />
+	<link id="shortcodes-css" rel="stylesheet" href="shortcodes.css" type="text/css" media="all"/>
+	<link id="skin-css" rel="stylesheet" href="skins/skyblue/style.css" type="text/css" media="all"/>
+    <link id="layer-slider" rel="stylesheet"  href="css/layerslider.css" media="all" />
+	
+	<!-- **Additional - stylesheets** -->
+	<link rel="stylesheet" href="responsive.css" type="text/css" media="all"/>
+	<link rel="stylesheet" href="css/meanmenu.css" type="text/css" media="all"/>
+	<link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen"/>
+    <link rel="stylesheet" href="css/animations.css" type="text/css" media="all" />
+    
+    <!-- **Font Awesome** -->
+	<link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+    
+    <!-- **Google - Fonts** -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>  
+    <link rel="stylesheet" href="css/fonts.css" />
+
+    <!-- **Custom stylesheets** -->
+    <link rel="stylesheet"  href="css/custom.css" media="all" />
+    <link rel="stylesheet"  href="css/czsk-wiki-font.css" media="all" />
+    
+</head>
+
+<body>
+<div class="loader-wrapper">
+	<div id="loader-image"></div>
+</div>
+<!-- **Wrapper** -->
+<div class="wrapper"> 
+	<div class="inner-wrapper">
+    	
+        <!-- **Header** -->
+
+            <?php include ("contents/header.html"); ?>
+
+            <!-- **Header - End** -->
+        </div>
+            
+        <!-- **Main - Starts** --> 
+		<div id="main">
+
+            <div class="dt-sc-margin75"></div>
+            <div class="dt-sc-hr-invisible"></div>
+			<div class="container">
+                <!-- Primary Starts -->
+                <section id="primary" class="content-full-width">
+                    <div class="error-info">
+						<h2> 404 - Stránka nebyla nalezena </h2>
+						<h3> Vraťte se na předchozí stránku nebo si vyberte téma z hlavního menu. </h3>
+					</div>
+                    
+                </section> <!-- Primary Ends -->
+            </div>  
+		
+        </div> <!-- **Main - Ends** --> 
+        
+    </div><!-- **inner-wrapper - End** -->
+    
+</div><!-- **Wrapper - End** -->
+        
+	<!-- **jQuery** -->  
+	<script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script src="js/jquery-migrate.min.js"></script>
+    
+    <script src="js/preloader.js" type="text/javascript"></script>
+    <script src="js/pace.min.js" type="text/javascript"></script>
+    
+    <script src="js/jquery.tabs.min.js"></script>
+    <script src="js/jquery.tipTip.minified.js"></script>
+	
+	<script src="js/jquery-easing-1.3.js" type="text/javascript"></script>
+    
+    <script src="js/jquery.parallax-1.1.3.js" type="text/javascript"></script>
+    
+    <script src="js/jquery.carouFredSel-6.2.1-packed.js" type="text/javascript"></script>
+    
+    <script src="js/jquery.inview.js" type="text/javascript"></script>
+    
+    <script src="js/jquery.bxslider.min.js" type="text/javascript"></script>
+	
+	<script src="js/jquery.nav.js" type="text/javascript"></script>
+    
+    <script src="js/jquery.donutchart.js" type="text/javascript"></script>
+	
+	<script src="js/jquery.jcarousel.min.js" type="text/javascript"></script>
+	
+    <script src="js/jquery.isotope.min.js" type="text/javascript"></script>
+	<script src="js/jquery.prettyPhoto.js" type="text/javascript"></script>
+	
+	<script src="js/masonry.pkgd.min.js" type="text/javascript"></script>
+    
+    <script src="js/jquery.smartresize.js" type="text/javascript"></script>
+	
+	<script src="js/responsive-nav.js" type="text/javascript"></script>
+	<script src="js/jquery.meanmenu.min.js" type="text/javascript"></script>
+	
+    <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+	<script src="js/jquery.gmap.min.js"></script>
+    
+	<!-- **Sticky Nav** -->
+	<script src="js/jquery.sticky.js" type="text/javascript"></script>
+    
+	<!-- **To Top** -->
+	<script src="js/jquery.ui.totop.min.js" type="text/javascript"></script>
+	
+	<script type="text/javascript" src="js/twitter/jquery.tweet.min.js"></script>
+    
+    <script src="js/jquery.viewport.js" type="text/javascript"></script> 
+	
+	<script type="text/javascript" src="js/jquery.validate.min.js"></script>
+	
+	<script src="js/retina.js" type="text/javascript"></script>
+	
+	<script src="js/jquery.nicescroll.min.js" type="text/javascript"></script>
+	<script src="js/custom.js" type="text/javascript"></script>       
+        
+</body>
+</html>
